ad1864a635a7bc58b609754ef40f519018c84165
paul yoel antezana <paul.antezana.2@gmail.com>
2019-12-06 18:13:51 -0500

    new updates

--- sky/script.js
+++ sky/script.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
+var SkyGuide;
 
-    (() => {
-        const SkyGuideTemplate = `<div class="SkyGuide">
+(() => {
+    const SkyGuideTemplate = `<div class="SkyGuide">
             <div class="SkyGuide-cover">{{modal-cover}}</div>
             <div class="SkyGuide-action">
                 <span class="SkyGuide-btn">{{modal-map}}</span>
@@ -29,95 +29,123 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
         </div>`;
 
-        const SkyGuideLang = {
-            lang: 'es',
-            cancelTitle: 'Cancelar',
-            cancelText: '×',
-            hideText: 'Ocultar',
-            tourMapText: '≡',
-            tourMapTitle: 'Mapa del gira',
-            nextTextDefault: 'Sig',
-            prevTextDefault: 'Ant',
-            endText: 'Fin',
 
-            modalIntroType: 'Introducción',
-            modalContinueType: 'Tour sin terminar',
+    const SkyGuideLang = {
+        lang: 'es',
+        cancelTitle: 'Cancelar',
+        cancelText: '×',
+        hideText: 'Ocultar',
+        tourMapText: '≡',
+        tourMapTitle: 'Mapa del gira',
+        nextTextDefault: 'Sig',
+        prevTextDefault: 'Ant',
+        endText: 'Fin',
 
-            contDialogBtnBegin: 'Primero',
-            contDialogBtnContinue: 'Continuar',
+        modalIntroType: 'Introducción',
+        modalContinueType: 'Tour sin terminar',
 
-            introDialogBtnStart: 'Start',
-            introDialogBtnCancel: 'Cancelar'
-        }
+        contDialogBtnBegin: 'Primero',
+        contDialogBtnContinue: 'Continuar',
 
-        let view,
-            step,
-            timer,
-            event,
-            data = {};
+        introDialogBtnStart: 'Start',
+        introDialogBtnCancel: 'Cancelar'
+    }
 
-        function getElementByContent(rootElement, text) {
-            let filter = {
-                acceptNode: function (node) {
-                    if (node.nodeType === document.TEXT_NODE && node.nodeValue.includes(text)) {
-                        return NodeFilter.FILTER_ACCEPT;
-                    }
-                    return NodeFilter.FILTER_REJECT;
+    function getElementByContent(rootElement, text) {
+        let filter = {
+            acceptNode: function (node) {
+                if (node.nodeType === document.TEXT_NODE && node.nodeValue.includes(text)) {
+                    return NodeFilter.FILTER_ACCEPT;
                 }
+                return NodeFilter.FILTER_REJECT;
             }
-            let nodes = [];
-            let walker = document.createTreeWalker(rootElement, NodeFilter.SHOW_TEXT, filter, false);
-            while (walker.nextNode()) {
-                nodes.push(walker.currentNode.parentNode);
-            }
-            return nodes;
+        }
+        let nodes = [];
+        let walker = document.createTreeWalker(rootElement, NodeFilter.SHOW_TEXT, filter, false);
+        while (walker.nextNode()) {
+            nodes.push(walker.currentNode.parentNode);
+        }
+        return nodes;
+    }
+
+    const getLastStandarElement = (elements, classNames = [], content = '') => {
+        let lastElement = elements[elements.length - 1];
+        lastElement.innerHTML = content;
+        classNames.forEach(item => lastElement.classList.add(item));
+        return lastElement;
+    }
+
+    const saveState = state => {
+        window.localStorage.setItem('SkyGuide', JSON.stringify(state));
+    }
+
+    const getState = () => {
+        let skyGuide = window.localStorage.getItem('SkyGuide');
+        return skyGuide != null ? JSON.parse(skyGuide) : null;
+    }
+
+    const destroyState = () => {
+        window.localStorage.removeItem('SkyGuide');
+    }
+
+    SkyGuide = (options = null) => {
+        let step,   // 1 - 2 - 3 - 4
+            event,  // create || start
+            data = {};
+        if (getState() == null && options == null) {
+            return;
         }
 
-        const getLastStandarElement = (elements, classNames = [], content = '') => {
-            let lastElement = elements[elements.length - 1];
-            lastElement.innerHTML = content;
-            classNames.forEach(item => lastElement.classList.add(item));
-            return lastElement;
+        if (getState() != null) {
+            let oldState = getState();
+            step = oldState.step;
+            event = oldState.event;
+            data = oldState.data;
+        } else {
+            step = 0;
+            event = 'create';
+            data = options;
         }
 
-        const saveState = state => {
-            window.localStorage.setItem('SkyGuide', JSON.stringify(state));
-        }
+        let skyGuideOverLay = document.createElement('div');
+        skyGuideOverLay.classList.add('SkyGuide-overlay');
 
-        let akyGuideModalPos = document.createElement('div');
-        akyGuideModalPos.classList.add('skyGuideModal-pos');
-        akyGuideModalPos.innerHTML = SkyGuideTemplate;
-        document.body.appendChild(akyGuideModalPos);
+        let skyGuideModalPos = document.createElement('div');
+        skyGuideModalPos.classList.add('SkyGuide-wrapper');
 
-        let sgModalCover = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-cover}}'), ['sgModal-cover'], '');
-        let sgModalTitle = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-title}}'), ['sgModal-title'], '');
-        let sgModalMap = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-map}}'), ['sgModal-map'], SkyGuideLang.tourMapText);
-        let sgModalClose = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-close}}'), ['sgModal-close'], SkyGuideLang.cancelText);
+        document.body.appendChild(skyGuideOverLay);
+        document.body.appendChild(skyGuideModalPos);
 
-        let sgModalHeader = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-header}}'), ['sgModal-header'], '');
-        let sgModalBody = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-body}}'), ['sgModal-body'], '');
+        skyGuideModalPos.innerHTML = SkyGuideTemplate;
+        let sgModalCover = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-cover}}'), ['sgModal-cover'], '');
+        let sgModalTitle = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-title}}'), ['sgModal-title'], '');
+        let sgModalMap = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-map}}'), ['sgModal-map'], SkyGuideLang.tourMapText);
+        let sgModalClose = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-close}}'), ['sgModal-close'], SkyGuideLang.cancelText);
 
-        let sgModalValue = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{step-value}}'), ['sgModal-value'], '');
-        let sgModalTotal = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{step-total}}'), ['sgModal-total'], '');
+        let sgModalHeader = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-header}}'), ['sgModal-header'], '');
+        let sgModalBody = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-body}}'), ['sgModal-body'], '');
 
-        let sgModalPrev = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-prev}}'), ['sgModal-prev'], SkyGuideLang.prevTextDefault);
-        let sgModalNext = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-next}}'), ['sgModal-next'], SkyGuideLang.nextTextDefault);
+        let sgModalValue = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{step-value}}'), ['sgModal-value'], '');
+        let sgModalTotal = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{step-total}}'), ['sgModal-total'], '');
 
-        let sgModalCancel = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-cancel}}'), ['sgModal-cancel'], SkyGuideLang.introDialogBtnCancel);
-        let sgModalStart = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-start}}'), ['sgModal-start'], SkyGuideLang.introDialogBtnStart);
+        let sgModalPrev = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-prev}}'), ['sgModal-prev'], SkyGuideLang.prevTextDefault);
+        let sgModalNext = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-next}}'), ['sgModal-next'], SkyGuideLang.nextTextDefault);
 
-        let sgModalFirst = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-first}}'), ['sgModal-first'], SkyGuideLang.contDialogBtnBegin);
-        let sgModalContinue = getLastStandarElement(getElementByContent(akyGuideModalPos, '{{modal-continue}}'), ['sgModal-continue'], SkyGuideLang.contDialogBtnContinue);
+        let sgModalCancel = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-cancel}}'), ['sgModal-cancel'], SkyGuideLang.introDialogBtnCancel);
+        let sgModalStart = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-start}}'), ['sgModal-start'], SkyGuideLang.introDialogBtnStart);
+
+        let sgModalFirst = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-first}}'), ['sgModal-first'], SkyGuideLang.contDialogBtnBegin);
+        let sgModalContinue = getLastStandarElement(getElementByContent(skyGuideModalPos, '{{modal-continue}}'), ['sgModal-continue'], SkyGuideLang.contDialogBtnContinue);
 
         const PaintSkyGuideModal = (modalContent, step, total) => {
-            console.log('**PAINT**',step, total);
-            if(step === -1){
+            console.log('**PAINT**', step, total);
+            if (step === -1) {
                 sgModalHeader.innerHTML = '';
                 sgModalBody.innerHTML = '';
             } else {
                 sgModalHeader.innerHTML = modalContent.title;
                 sgModalBody.innerHTML = modalContent.content;
-    
+
                 if (step === 0) {
                     sgModalPrev.classList.add('sgIsHide');
                     sgModalNext.classList.add('sgIsHide');
@@ -143,35 +171,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
                     sgModalFirst.classList.add('sgIsHide');
                     sgModalContinue.classList.add('sgIsHide');
+
+                    saveState({
+                        step,
+                        event,
+                        data,
+                    });
                 }
             }
-
-            saveState({
-                view,
-                step,
-                timer,
-                event,
-                data,
-            });
         }
 
-        const handleRun = options => {
-            data = options;
+        const destroyGuide = () => {
+            skyGuideOverLay.remove();
+            skyGuideModalPos.remove();
+            destroyState();
+        }
+
+        console.log(step);
+
+        if(step >= 1){
             PaintSkyGuideModal({
-                ...data.intro
+                ...data.steps[step - 1],
+            }, step, data.steps.length);
+        } else {
+            PaintSkyGuideModal({
+                ...data.intro,
             }, 0, 0);
-        };
+        }
 
         const handleStart = () => {
-            console.log('handleStart');
-            step = 0;
+            step = 1;
             PaintSkyGuideModal({
-                ...data.steps[step],
-            }, step + 1, data.steps.length);
+                ...data.steps[step - 1],
+            }, step, data.steps.length);
         };
 
         const handleCancel = () => {
-            console.log('handleCancel');
+            destroyGuide();
         };
 
         const handleContinue = () => {
@@ -179,47 +215,49 @@ document.addEventListener("DOMContentLoaded", () => {
         };
 
         const handleFirst = () => {
-            console.log('handleFirst');
-            step = 0;
+            step = 1;
             PaintSkyGuideModal({
-                ...data.steps[step],
-            }, step + 1, data.steps.length);
+                ...data.steps[step-1],
+            }, step, data.steps.length);
         };
 
         const handlePrev = () => {
-            console.log('handlePrev');
             step = step - 1;
             PaintSkyGuideModal({
-                ...data.steps[step],
-            }, step + 1, data.steps.length);
+                ...data.steps[step-1],
+            }, step, data.steps.length);
         };
 
         const handleNext = () => {
-            console.log('handleNext');
-            step = ((data.steps.length - 1) == step) ? step : step  + 1;
-            PaintSkyGuideModal({
-                ...data.steps[step],
-            }, step + 1, data.steps.length);
+            if(data.steps.length == step){
+                step = 0;
+                destroyGuide();
+            } else {
+                step = step + 1;
+                PaintSkyGuideModal({
+                    ...data.steps[step-1],
+                }, step, data.steps.length);
+            }
         };
 
 
+        sgModalClose.addEventListener('click', handleCancel);
         sgModalPrev.addEventListener('click', handlePrev);
         sgModalNext.addEventListener('click', handleNext);
         sgModalCancel.addEventListener('click', handleCancel);
         sgModalStart.addEventListener('click', handleStart);
         sgModalFirst.addEventListener('click', handleFirst);
         sgModalContinue.addEventListener('click', handleContinue);
+    }
 
-        let SkyGuide = {
-            run(options){
-                handleRun(options);
-            }
-        }
-
-        window.SkyGuide = SkyGuide;
-    })();
+    document.addEventListener("DOMContentLoaded", () => {
+        SkyGuide();
+    });
+})();
 
 
+// Use app
+document.addEventListener("DOMContentLoaded", () => {
     let data = {
         tourId: '1',
         intro: {
@@ -264,28 +302,9 @@ document.addEventListener("DOMContentLoaded", () => {
         ],
     }
 
-    SkyGuide.run(data);
+
+    let StartExample = document.getElementById('StartExample');
+    StartExample.addEventListener('click', () => {
+        SkyGuide(data);
+    });
 });
-
-
-
-
-
-
-
-
-//let runescape = new RegExp("\\\\[\\da-fA-F]{1,6}" + whitespace + "?|\\\\([^\\r\\n\\f])", "g");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
